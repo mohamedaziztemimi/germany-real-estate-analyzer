45c17952f1dc9621ad3bc8aafb32d85ae6a46b7a
mohamedaziztemimi <mohamedaziz.temimi@esprit.tn>
2026-01-07 18:35:38 +0100

    adding ocnfig

--- next.config.mjs
+++ next.config.mjs
@@ -3,9 +3,12 @@ const nextConfig = {
   typescript: { ignoreBuildErrors: true },
   images: { unoptimized: true },
   turbopack: { root: "." },
+
   async rewrites() {
     const target =
-      process.env.API_REWRITE_TARGET || "https://site--readlestateai--8r2ddr2v5b7f.code.run/";
+      process.env.API_REWRITE_TARGET ||
+      "https://site--readlestateai--8r2ddr2v5b7f.code.run";
+
     return [
       {
         source: "/api/:path*",
